a23bbd91982aad7cc85e0e96a63407c0e4c6f316
chinhdq <chinhduongquoc@gmail.com>
2018-10-18 10:26:50 +0700

    Init

---
+++ dqc.lib/dqc.lib/Operation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dqc.lib
+{
+    public class Operation
+    {
+        public int Add(int operand1, int operand2)
+        {
+            return operand1 + operand2;
+        }
+    }
+}
